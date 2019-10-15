7f2d42755b00f339f96f27b1a978446059008e60
zawa-works <thestoryidontknow@gmail.com>
2019-10-15 20:19:23 +0900

    selected image has border

--- settings.js
+++ settings.js
@@ -1,1 +1,14 @@
+const ids = ['very_satisfied','satisfied','neither','dissatisfied','very_dissatisfied']
+
 document.addEventListener('touchmove', function(e) {e.preventDefault();}, {passive: false});
+function drawSelectedBorder(ele){
+    let id_value = ele.id
+    ids.forEach(function(id){
+        let obj = document.getElementById(id)
+        if(id === id_value){
+            obj.style.border = "solid 1px yellow";
+        }else{
+            obj.style.borderWidth = "0px";
+        }
+    })
+}
