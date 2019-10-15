d63d575c66d63e52e2a04d8c140cc9685717c21b
zawa-works <thestoryidontknow@gmail.com>
2019-10-15 20:31:29 +0900

    remove extra files

--- settings.js
+++ settings.js
@@ -1,14 +1,13 @@
-const ids = ['very_satisfied','satisfied','neither','dissatisfied','very_dissatisfied']
+const ids = ['very_satisfied', 'satisfied', 'neither', 'dissatisfied', 'very_dissatisfied']
 
-document.addEventListener('touchmove', function(e) {e.preventDefault();}, {passive: false});
-function drawSelectedBorder(ele){
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, { passive: false });
+function drawSelectedBorder(ele) {
     let id_value = ele.id
-    ids.forEach(function(id){
+    ids.forEach(function (id) {
         let obj = document.getElementById(id)
-        if(id === id_value){
-            obj.style.border = "solid 1px yellow";
-        }else{
-            obj.style.borderWidth = "0px";
-        }
+        if (id === id_value)
+            obj.style.border = 'solid 1px yellow';
+        else
+            obj.style.borderWidth = '0px';
     })
 }
